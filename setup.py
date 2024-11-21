4fd44ebb2a1bc2c564354a75ecc5b31ad8917ee4
1jamescaldwell <ywe4kw@virginia.edu>
2024-11-21 16:05:13 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='MC_Package',
+    version='0.1',
+    description='A Monte Carlo Simulator Package',
+    url='https://github.com/1jamescaldwell/Monte-Carlo-Simulator',
+    author='James Caldwell',
+    author_email='ywe4kw@virginia.edu',
+    license='MIT',
+    packages=['MC_Package']
+)
+
